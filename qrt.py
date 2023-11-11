2176b46b1303e9a5b64e3021842b8f6e5ba1dd5d
Yashawanth R <89150253+yashu1wwww@users.noreply.github.com>
2023-11-11 19:55:07 +0530

    Update qrt.py

--- qrt.py
+++ qrt.py
@@ -1,147 +1,58 @@
-import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+import time
+import random
+
+commentsDict = ['good','amazing one','keep going','excellent','next video please','sub to your channel','shared to others','made my day','keep it up','sensational','rock it','challenge it','post video daily','work was amazing','needed more edit','edit was awesome',
+'what a video man','watched yesterday','your are genious','faster than light','your work needed success','new fan of you','keep rock dude','copy cat','link the video','listening','writing','reading','playing',] #replace with your words
 
 driver= webdriver.Chrome()
 driver.maximize_window()
 
 driver.get("https://twitter.com/i/flow/login")
-time.sleep(10)
+time.sleep(6)
 email = driver.find_element_by_name('text')
-email.send_keys("123_tweet") #replace with your twitter account & aslo replace in below where username asked
+email.send_keys("FUser73216") #replace with your twitter account username
 email.send_keys(Keys.ENTER)
 time.sleep(2)
-password = driver.find_element_by_name("password") #replace with your twitter password & also  replace in below where username asked
-password.send_keys("pass1@#$")
+password = driver.find_element_by_name("password") #replace with your twitter password 
+password.send_keys("Twitter_123")
 password.send_keys(Keys.ENTER)
+time.sleep(4)
+driver.get("https://twitter.com/Kannada_KBO_/status/1723223451043799453") #replace with which post you want auto quote..
 time.sleep(5)
 
-with open("urls.txt") as f:
-    for url in f:
-        driver.get(url) 
+counter = 0
+while True:
+    try:
+        
+        # Post the tweet
+        driver.find_element_by_xpath("//div[@data-testid='retweet']/div").click() #retweet button
+        time.sleep(1)
+        #click on quote button
+        driver.execute_script('document.querySelector("#layers > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-105ug2t.r-u8s1d.r-zchlnj.r-ipm5af > div > div > div > div:nth-child(2) > div > div.css-1dbjc4n.r-14lw9ot.r-1q9bdsx.r-1upvrn0.r-j2cz3j.r-1udh08x.r-u8s1d > div > div > div > a:nth-child(2) > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2 > div > span").click()')
+        time.sleep(1)
+        driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(random.choice(commentsDict)) #randomly cmt
+        time.sleep(1)
+        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/span/span').click() #click on post button
+        time.sleep(1)  
 
-time.sleep(8)
-
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"Somes tweet") #replace with your word do in below also..
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"true tweet")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"true tweeted now")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"best one")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"amazing")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.close()
-
-driver= webdriver.Chrome()
-driver.maximize_window()
-
-driver.get("https://twitter.com/i/flow/login")
-time.sleep(10)
-email = driver.find_element_by_name('text')
-email.send_keys("123_tweet")
-email.send_keys(Keys.ENTER)
+           
+        counter += 1
+        if counter == 4: #Change '5' to 'how many auto tweet for a acc....
+            break
+        
+    except Exception as e:
+        print("An error occurred:", e)
+        break  
+        
 time.sleep(2)
-password = driver.find_element_by_name("password")
-password.send_keys("pass1@#$")
-password.send_keys(Keys.ENTER)
-time.sleep(5)
-
-with open("urls.txt") as f:
-    for url in f:
-        driver.get(url) 
-
-time.sleep(8)
-
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"Somes tweet")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"true tweet")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"true tweeted now")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"best one")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"amazing")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.close()
-
-driver= webdriver.Chrome()
-driver.maximize_window()
-
-driver.get("https://twitter.com/i/flow/login")
-time.sleep(10)
-email = driver.find_element_by_name('text')
-email.send_keys("123_tweet")
-email.send_keys(Keys.ENTER)
-time.sleep(2)
-password = driver.find_element_by_name("password")
-password.send_keys("pass@#$")
-password.send_keys(Keys.ENTER)
-time.sleep(5)
-
-with open("urls.txt") as f:
-    for url in f:
-        driver.get(url) 
-
-time.sleep(8)
-
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"Somes tweet")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"true tweet")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"true tweeted now")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"best one")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.implicitly_wait(4)
-retweet_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Retweet']"))).click()
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/a/div[2]/div/span').click()
-driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(f"amazing")
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click() 
-driver.close()
-
-
-
