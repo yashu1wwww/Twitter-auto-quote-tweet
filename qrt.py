c6d0314e03acc51a18f614978ab79c9d6c72e281
Yashwanth R <89150253+yashu1wwww@users.noreply.github.com>
2025-03-02 08:39:58 +0530

    Update qrt.py

--- qrt.py
+++ qrt.py
@@ -1,59 +1,103 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import ElementClickInterceptedException
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import random
 
-commentsDict = ['good','amazing one','keep going','excellent','next video please','sub to your channel','shared to others','made my day','keep it up','sensational','rock it','challenge it','post video daily','work was amazing','needed more edit','edit was awesome',
-'what a video man','watched yesterday','your are genious','faster than light','your work needed success','new fan of you','keep rock dude','copy cat','link the video','listening','writing','reading','playing',] #replace with your words
+# List of Twitter accounts
+accounts = [
+    {"username": "john_doe123", "password": "Pass@1234"},
+    {"username": "alice_smith99", "password": "Alice@456"},
+    {"username": "michael_brown", "password": "Mike@789"}
+]  # Add more accounts as needed
 
-driver= webdriver.Chrome()
-driver.maximize_window()
+# Comments to post
+commentsDict = [
+    'good', 'amazing one', 'keep going', 'excellent', 'next video please', 'sub to your channel',
+    'shared to others', 'made my day', 'keep it up', 'sensational', 'rock it', 'challenge it',
+    'post video daily', 'work was amazing', 'needed more edit', 'edit was awesome', 'what a video man',
+    'watched yesterday', 'you are genius', 'faster than light', 'your work needs success',
+    'new fan of you', 'keep rocking dude', 'copy cat', 'link the video', 'listening', 'writing',
+    'reading', 'playing'
+]  
 
-driver.get("https://twitter.com/i/flow/login")
-time.sleep(6)
-email = driver.find_element_by_name('text')
-email.send_keys("Fmsong@123") #replace with your twitter account username
-email.send_keys(Keys.ENTER)
-time.sleep(2)
-password = driver.find_element_by_name("password") 
-password.send_keys("Twitter123") #replace with your twitter password 
-password.send_keys(Keys.ENTER)
-time.sleep(4)
+# Tweet URL to quote
+tweet_url = "https://twitter.com/hasantoxr/status/1723266777705001004"  # Change this
 
-driver.get("https://twitter.com/hasantoxr/status/1723266777705001004") #replace with which post you want auto quote..
-time.sleep(5)
+# Number of quote tweets per account
+num_quotes_per_account = 4  
 
-counter = 0
-while True:
+for account in accounts:
     try:
-        
-        # Post the tweet
-        driver.find_element_by_xpath("//div[@data-testid='retweet']/div").click() #retweet button
-        time.sleep(1)
-        #click on quote button
-        driver.execute_script('document.querySelector("#layers > div.css-1dbjc4n.r-1p0dtai.r-1d2f490.r-105ug2t.r-u8s1d.r-zchlnj.r-ipm5af > div > div > div > div:nth-child(2) > div > div.css-1dbjc4n.r-14lw9ot.r-1q9bdsx.r-1upvrn0.r-j2cz3j.r-1udh08x.r-u8s1d > div > div > div > a:nth-child(2) > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2 > div > span").click()')
-        time.sleep(1)
-        driver.find_element_by_class_name('public-DraftStyleDefault-ltr').send_keys(random.choice(commentsDict)) #randomly cmt
-        time.sleep(1)
-        driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/span/span').click() #click on post button
-        time.sleep(1)  
+        # Open a new browser for each account
+        driver = webdriver.Chrome()
+        driver.maximize_window()
 
-           
-        counter += 1
-        if counter == 4: #Change '5' to 'how many auto tweet for a acc....
-            break
-        
+        # Open Twitter login page
+        driver.get("https://twitter.com/i/flow/login")
+        time.sleep(6)
+
+        # Enter Username
+        email = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "text")))
+        email.send_keys(account["username"])
+        email.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        # Enter Password
+        password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
+        password.send_keys(account["password"])
+        password.send_keys(Keys.ENTER)
+        time.sleep(4)
+
+        # Open the tweet to quote
+        driver.get(tweet_url)
+        time.sleep(5)
+
+        # Quote tweet multiple times
+        counter = 0
+        while counter < num_quotes_per_account:
+            try:
+                # Click Retweet Button
+                retweet_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='retweet']"))
+                )
+                retweet_button.click()
+                time.sleep(1)
+
+                # Click Quote Tweet Button
+                quote_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, "//span[text()='Quote']"))
+                )
+                quote_button.click()
+                time.sleep(1)
+
+                # Enter Random Comment
+                comment_box = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Post text']"))
+                )
+                comment_box.send_keys(random.choice(commentsDict))
+                time.sleep(1)
+
+                # Click Post Button
+                post_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, '//span[text()="Post"]'))
+                )
+                post_button.click()
+                time.sleep(1)
+
+                counter += 1
+
+            except Exception as e:
+                print(f"Error while quoting tweet for {account['username']}: {e}")
+                break
+
+        # Close the browser after completing the task
+        driver.quit()
+        time.sleep(3)  # Small delay before switching to the next account
+
     except Exception as e:
-        print("An error occurred:", e)
-        break  
-        
-time.sleep(2)
+        print(f"Error with account {account['username']}: {e}")
+
+print("Task completed for all accounts.")
